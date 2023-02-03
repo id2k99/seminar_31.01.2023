1d15b286a82d124ec6b4eea93a6b0de0aee05499
sergey <sergey@solaev.ru>
2023-02-03 19:26:05 +0400

    Задачи

---
+++ 12/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+//Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+Console.Clear();
+Console.WriteLine("Enter first number: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+if ((num1 % num2) == 0 ){
+    Console.WriteLine($"{num1} кратно {num2}");
+}
+else {
+    Console.WriteLine(num1 % num2);
+}
---
+++ 9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+int num = new Random().Next(10, 100);
+
+int num1 = num / 10;
+int num2 = num % 10;
+int max =num2;
+if (num1 > num2) {
+    max = num1;
+    Console.WriteLine($" {num} Максимальное цифра числа {max}");
+}
+
+else {
+    Console.WriteLine($"{num} Максимальное цифра числа {max}");
+}
